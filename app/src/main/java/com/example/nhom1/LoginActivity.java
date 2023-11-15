89d1400c03bb949a37adc3be12a08ee073c262b3
unknown <quylvph19871@gmail.com>
2023-11-16 00:43:27 +0700

    cơ sở dữ liệu

--- app/src/main/java/com/example/nhom1/LoginActivity.java
+++ app/src/main/java/com/example/nhom1/LoginActivity.java
@@ -1,17 +1,17 @@
 package com.example.nhom1;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_login);
 
     }
 }
