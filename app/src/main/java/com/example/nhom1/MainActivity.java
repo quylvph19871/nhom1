e3350abf9ee15ea67a154bf8873836eec93431f7
LUU QUY <quylvph19871@gmail.com>
2023-11-16 17:04:22 +0700

    banner

--- app/src/main/java/com/example/nhom1/MainActivity.java
+++ app/src/main/java/com/example/nhom1/MainActivity.java
@@ -17,11 +17,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     public static BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView =findViewById(R.id.bottomnavigation);
+        bottomNavigationView = findViewById(R.id.bottomnavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.Trangchu);
         load(new HomeFragment());
@@ -31,10 +32,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment fragment;
-        if (item.getItemId() == R.id.Trangchu){
-        fragment= new HomeFragment();
-        load(fragment);
-        return true;
+        if (item.getItemId() == R.id.Trangchu) {
+            fragment = new HomeFragment();
+            load(fragment);
+            return true;
         } else if (item.getItemId() == R.id.gioHang) {
             fragment = new GioHangFragment();
             load(fragment);
@@ -43,16 +44,16 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             fragment = new DonHangFragment();
             load(fragment);
             return true;
-        }else if (item.getItemId() == R.id.taikhoan){
+        } else if (item.getItemId() == R.id.taikhoan) {
             fragment = new TaiKhoanFragment();
             load(fragment);
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    public void load(Fragment fragment){
+    public void load(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, fragment);
         fragmentTransaction.addToBackStack(null);
